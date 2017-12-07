a755ead6535a05296bd729c970a33e7faa69bd09
罗勇 <luoyong@13322ty.com>
2017-12-07 12:00:00 +0800

    整理提交

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# nice-spring-session
-web工程 利用spring-session 共享session到 redis cluster
+
+## 分布式web工程利用 spring-session 共享 session 到 redis cluster
+
+使用方法如下：
+
+pom.xml 添加依赖
+
+    <dependency>
+        <groupId>com.nice</groupId>
+        <artifactId>nice-spring-session</artifactId>
+        <version>1.0.0-RELEASE</version>
+    </dependency>
+
+
+web.xml 中配置过滤器
+
+    <filter>
+        <filter-name>springSessionRepositoryFilter</filter-name>
+        <filter-class>org.springframework.web.filter.DelegatingFilterProxy</filter-class>
+    </filter>
+    <filter-mapping>
+        <filter-name>springSessionRepositoryFilter</filter-name>
+        <url-pattern>/*</url-pattern>
+    </filter-mapping>
+
+<br/><br/>
+下面两个是可选的配置
+
+#session 存活时间，默认是30分钟<br/>
+session.maxSeconds=1800<br/>
+#session 存储在redis中的命名空间<br/>
+session.redisNamespace=a\:b\:c<br/>
---
+++ src/main/java/com/nice/spring/session/RedisHttpSessionCfg.java
@@ -0,0 +1,66 @@
+
+package com.nice.spring.session;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+/**
+ * Redis初始化
+ * @author Luo Yong
+ * @date 2017-03-12
+ */
+@Component
+public class RedisHttpSessionCfg {
+
+	/**
+	 * session 在 redis cluster 缓存中最长时间<br/>
+	 * 可以不设置，默认值是1800秒
+	 */
+	@Value("${session.maxSeconds}")
+	private Integer maxInactiveIntervalInSeconds;
+	/**
+	 * redis namespace <br/>
+	 * 可以不设置
+	 */
+	@Value("${session.redisNamespace}")
+	private String redisNamespace;
+
+	/**
+	 * 初始化 redisHttpSessionConfiguration注入bean到Spring
+	 * @return
+	 */
+	@Bean
+	public RedisHttpSessionConfiguration redisHttpSessionConfiguration() {
+		RedisHttpSessionConfiguration redisHttpSessionConfiguration = new RedisHttpSessionConfiguration();
+		if (getMaxInactiveIntervalInSeconds() != null) {
+			redisHttpSessionConfiguration.setMaxInactiveIntervalInSeconds(getMaxInactiveIntervalInSeconds());
+		}
+		String namespace = getRedisNamespace();
+		if (namespace != null && !StringUtils.isEmpty(namespace.trim())) {
+			redisHttpSessionConfiguration.setRedisNamespace(namespace.trim());
+		}
+		return redisHttpSessionConfiguration;
+	}
+
+	public Integer getMaxInactiveIntervalInSeconds() {
+		return maxInactiveIntervalInSeconds;
+	}
+
+	public void setMaxInactiveIntervalInSeconds(Integer maxInactiveIntervalInSeconds) {
+		this.maxInactiveIntervalInSeconds = maxInactiveIntervalInSeconds;
+	}
+
+	public String getRedisNamespace() {
+		return redisNamespace;
+	}
+
+	public void setRedisNamespace(String redisNamespace) {
+		this.redisNamespace = redisNamespace;
+	}
+
+	private RedisHttpSessionCfg() {
+	}
+}
